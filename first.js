d0c5f85c0f63ccdf9eff000d36c62b52fd4ff239
zaid <zaidbathspa.ae@gmail.com>
2023-06-01 00:03:15 +0400

    completed

---
+++ first.js
@@ -0,0 +1,111 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+/**
+ * addd event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400 ? header.classList.add("active")
+    : header.classList.remove("active");
+}); 
+
+
+
+
+
+
+
+
+const items = document.querySelectorAll('.item')
+
+const expand = (item, i) => {
+  items.forEach((it, ind) => {
+    if (i === ind) return
+    it.clicked = false
+  })
+  gsap.to(items, {
+    width: item.clicked ? '15vw' : '8vw',
+    duration: 2,
+    ease: 'elastic(1, .6)'
+  })
+  
+  item.clicked = !item.clicked
+  gsap.to(item, {
+    width: item.clicked ? '42vw' : '15vw',
+    duration: 2.5,
+    ease: 'elastic(1, .3)'
+  })
+}
+
+items.forEach((item, i) => {
+  item.clicked = false
+  item.addEventListener('click', () => expand(item, i))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
